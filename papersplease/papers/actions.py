a667a05f2fc230f2fc579861a78a52fcc0e38426
Daniel García Moreno <danigm@wadobo.com>
2017-04-16 12:58:45 +0200

    Added download action

--- papersplease/papers/actions.py
+++ papersplease/papers/actions.py
@@ -1,19 +1,15 @@
 from __future__ import unicode_literals
 
+import os
+import io
+import tarfile
+
+from django.http import HttpResponse
 from .models import PAPER_CHOICES
 from functools import partial
 
 
 paper_actions = []
-
-def set_status(modeladmin, request, queryset, key='missing'):
-    queryset.update(status=key)
-
-for k, v in PAPER_CHOICES:
-    action = partial(set_status, key=k)
-    action.short_description = u"Set status to '{0}'".format(v)
-    action.__name__ = 'set_{0}'.format(k)
-    paper_actions.append(action)
 
 
 email_body = '''
@@ -50,4 +46,43 @@ def send_email(modeladmin, request, queryset):
 
         p.email(subject, message)
 
+
+def download(modeladmin, request, queryset):
+    response = HttpResponse(content_type="application/gzip")
+    response['Content-Disposition'] = 'attachment; filename="papers.tar.gz"'
+
+    files = []
+    for p in queryset:
+        files += [i.attach.path for i in p.attachs.all()]
+
+    output = io.BytesIO()
+    tar = tarfile.open(fileobj=output, mode='w:gz')
+
+    def reset(tarinfo):
+        name = os.path.basename(tarinfo.name)
+        _, d = os.path.split(os.path.dirname(tarinfo.name))
+        tarinfo.name = os.path.join(d, name)
+        return tarinfo
+
+    for f in files:
+        tar.add(f, filter=reset)
+    tar.close()
+
+    output.seek(0)
+    response.content = output.read()
+
+    return response
+
+
+paper_actions.append(download)
 paper_actions.append(send_email)
+
+
+def set_status(modeladmin, request, queryset, key='missing'):
+    queryset.update(status=key)
+
+for k, v in PAPER_CHOICES:
+    action = partial(set_status, key=k)
+    action.short_description = u"Set status to '{0}'".format(v)
+    action.__name__ = 'set_{0}'.format(k)
+    paper_actions.append(action)
